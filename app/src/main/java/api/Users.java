e061e3c3e56d26b13d919395fb9a0982d75792c5
daniel-95 <deniel1495@mail.ru>
2016-02-29 16:42:57 +0300

    reg+proto api #2

---
+++ app/src/main/java/api/Users.java
@@ -0,0 +1,11 @@
+package api;
+
+/**
+ * Created by 1запуск BeCompact on 29.02.2016.
+ */
+public class Users {
+    private ServerConnection sConn;
+    public Users(ServerConnection sConn) {
+        this.sConn = sConn;
+    }
+}
---
+++ app/src/main/java/api/contacts.java
@@ -0,0 +1,7 @@
+package api;
+
+/**
+ * Created by 1запуск BeCompact on 29.02.2016.
+ */
+public class contacts {
+}
