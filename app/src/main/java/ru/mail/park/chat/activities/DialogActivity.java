e0003f4de39466538a8f9c1d78356ff3e52ada4c
Mihail Krutyakov <mihanik01@mail.ru>
2016-03-26 22:45:00 +0300

    fix R in dialog activity

--- app/src/main/java/ru/mail/park/chat/activities/DialogActivity.java
+++ app/src/main/java/ru/mail/park/chat/activities/DialogActivity.java
@@ -3,6 +3,8 @@ package ru.mail.park.chat.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ru.mail.park.chat.R;
+
 public class DialogActivity extends AppCompatActivity {
 
     @Override
