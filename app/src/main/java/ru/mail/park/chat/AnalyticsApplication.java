e8ca5ac26f5f4430c8c9a874d678dec2fd1b5a62
Михаил Крутяков <m.krutyakov@yandex.ru>
2016-08-02 20:45:19 +0300

    prepare for publication

--- app/src/main/java/ru/mail/park/chat/AnalyticsApplication.java
+++ app/src/main/java/ru/mail/park/chat/AnalyticsApplication.java
@@ -1,6 +1,9 @@
 package ru.mail.park.chat;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
@@ -8,8 +11,14 @@ import com.google.android.gms.analytics.Tracker;
 /**
  * Created by Михаил on 25.06.2016.
  */
-public class AnalyticsApplication extends Application {
+public class AnalyticsApplication extends MultiDexApplication {
     private Tracker mTracker;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 
     synchronized public Tracker getDefaultTracker() {
         if (mTracker == null) {
